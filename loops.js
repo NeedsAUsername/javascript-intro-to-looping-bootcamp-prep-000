3c8d898e5617aa5988d84daa9675f39afba3d8bd
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-21 22:54:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forloop(array) {
   for (let i = 0; i<25; i+=1) {
-    if (i = 1) {
+    if (i === 1) {
       array.push(`I am ${i} strange loop.`)
     }
     else {
     array.push(`I am ${i} strange loops.`)
   }
-}
+}}
 
 array = []
 
